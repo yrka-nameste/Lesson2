649521931d2766697a896294a26b744673bcb6e4
yrka-nameste <122967349+yrka-nameste@users.noreply.github.com>
2025-03-09 16:47:50 +0200

    HW2 Task4

---
+++ homework_2_4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_2_4
+{
+    interface IFileHandler
+    {
+        void Open(string filePath);
+    }
+
+    interface ITextReadable
+    {
+        void ReadText();
+    }
+
+    interface IAudioPlayable
+    {
+        void PlayAudio();
+    }
+
+    class TextFileHandler : IFileHandler, ITextReadable
+    {
+        public void Open(string filePath)
+        {
+            Console.WriteLine($"Открываем текстовый файл: {filePath}");
+        }
+
+        public void ReadText()
+        {
+            Console.WriteLine("Читаем текст из файла...");
+        }
+    }
+
+    class AudioFileHandler : IFileHandler, IAudioPlayable
+    {
+        public void Open(string filePath)
+        {
+            Console.WriteLine($"Открываем аудиофайл: {filePath}");
+        }
+
+        public void PlayAudio()
+        {
+            Console.WriteLine("Воспроизводим аудио...");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFileHandler textFile = new TextFileHandler();
+            IFileHandler audioFile = new AudioFileHandler();
+
+            textFile.Open("file.txt");
+            if (textFile is ITextReadable readable)
+            {
+                readable.ReadText();
+            }
+
+            audioFile.Open("song.mp3");
+            if (audioFile is IAudioPlayable playable)
+            {
+                playable.PlayAudio();
+            }
+        }
+    }
+}
