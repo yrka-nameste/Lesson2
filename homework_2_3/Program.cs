ac25b445c4b36aa4f1a6d01e56238a60e68156d8
yrka-nameste <122967349+yrka-nameste@users.noreply.github.com>
2025-03-09 15:49:14 +0200

    HW2 Task3

---
+++ homework_2_3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_2_3
+{
+    abstract class FileProcessor
+    {
+        public abstract void Upload(string filePath);
+    }
+
+    interface IConvertibleFile
+    {
+        void Convert(string filePath);
+    }
+
+    class PdfConverter : FileProcessor, IConvertibleFile
+    {
+        public override void Upload(string filePath)
+        {
+            Console.WriteLine($"Загружаем PDF-файл: {filePath}");
+        }
+
+        public void Convert(string filePath)
+        {
+            Console.WriteLine($"Конвертируем {filePath} в PDF...");
+        }
+    }
+
+    class EncryptedFile : FileProcessor
+    {
+        public override void Upload(string filePath)
+        {
+            Console.WriteLine($"Загружаем зашифрованный файл: {filePath}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FileProcessor> files = new List<FileProcessor>
+        {
+            new PdfConverter(),
+            new EncryptedFile()
+        };
+
+            foreach (var file in files)
+            {
+                file.Upload("document.txt");
+                if (file is IConvertibleFile convertible)
+                {
+                    convertible.Convert("document.txt");
+                }
+            }
+        }
+    }
+}
